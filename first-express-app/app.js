3b561f5373ae89ddbb4180d9de94ed98f59b05a5
Germán Álvarez <46670724+GermanAlvarez-IH@users.noreply.github.com>
2019-05-14 16:51:05 +0200

    documentacion

--- first-express-app/app.js
+++ first-express-app/app.js
@@ -1,1 +1,26 @@
+const express = require('express')
 
+const app = express()
+
+const port = 3000
+
+app.use(express.static('public'))
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home-page.html');
+})
+
+app.get('/cat', (req, res) => {
+    res.sendFile(__dirname + '/views/cat-page.html');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`App escuchando en el puerto ${port}`)
+});
